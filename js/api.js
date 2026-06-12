c21e2f4a4bf8aceafcc9d51b77e23cca6586e8f3
DuongNKT <nguyenkieutungduong@gmail.com>
2026-06-12 15:06:25 +0700

    design a navbar in index.html

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://dummyjson.com/products";
 
 async function getProducts() {
-  const response = await fetch(`${BASE_URL}?limit=100`);
+  const response = await fetch(`${BASE_URL}?limit=7`);
   if (!response.ok) {
     throw new Error("Failed to fetch products");
   }
